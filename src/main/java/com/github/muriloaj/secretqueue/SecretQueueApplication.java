98195b97f7602b561464989a077ca345bea22851
Murilo Julieti <murilo.julieti@certibio.com.br>
2018-06-15 15:26:30 -0300

    first

---
+++ src/main/java/com/github/muriloaj/secretqueue/SecretQueueApplication.java
@@ -0,0 +1,12 @@
+package com.github.muriloaj.secretqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretQueueApplication.class, args);
+	}
+}
