1b0a59aeae2227eed41e8635cf692ab992a65dfb
Murilo Julieti <murilo.julieti@certibio.com.br>
2018-06-18 16:23:25 -0300

    migrate to lazy mode using API

--- src/main/java/com/github/muriloaj/secretqueue/controller/API.java
+++ src/main/java/com/github/muriloaj/secretqueue/controller/API.java
@@ -1,16 +1,44 @@
 package com.github.muriloaj.secretqueue.controller;
 
-import com.github.muriloaj.secretqueue.Runner;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class API {
-    @RequestMapping(value = "send")
-    public String sendMessage(){
-        
+
+    private String queue = "secret-queue";
+
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
+
+    @RequestMapping(value = "send",
+            method = RequestMethod.POST)
+    public String sendMessage(@RequestBody String secret) {
+        ConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitHost);
+        AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareQueue(new Queue(queue));
+        AmqpTemplate template = new RabbitTemplate(connectionFactory);
+        template.convertAndSend(queue, secret.trim().concat(" at ").concat(new java.util.Date().toString()));
         return "OK";
+    }
+
+    @RequestMapping(value = "take")
+    public String takeMessage() {
+        ConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitHost);
+        AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareQueue(new Queue(queue));
+        AmqpTemplate template = new RabbitTemplate(connectionFactory);
+        return (String) template.receiveAndConvert(queue);
     }
 }
